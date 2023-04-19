0044387663a8d481ad725bcca72b439a6c8414e5
yeoni <siunaus224@gmail.com>
2023-04-19 17:53:38 +0900

    230419

---
+++ 230419/class/reduce.js
@@ -0,0 +1,5 @@
+const tests = [90, 90, 80, 77]
+
+const sum = tests.reduce(function (total, x) {
+  return total + x
+}, 0)
---
+++ 230419/index.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('button');
+
+btn.onmouseover = function() {
+  alert('누르지마세요!')
+}
